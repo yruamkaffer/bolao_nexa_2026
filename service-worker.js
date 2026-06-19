67f55648f7993fc8334ff04f99883b918e2e03db
Yruam Käffer de Faria <yruamkf@gmail.com>
2026-06-19 16:21:59 -0300

    final

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bolao-nexa-pwa-v5-fastcsv';
+const CACHE_NAME = 'bolao-nexa-pontos-v4';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png', '/icons/maskable-512.png'];
 
 self.addEventListener('install', event => {
