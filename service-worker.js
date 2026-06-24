96c660b61ae6149c830af1f19ceda7c3e2749578
Yruam Käffer de Faria <yruamkf@gmail.com>
2026-06-24 16:48:09 -0300

    3 FASE

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bolao-nexa-final-bracket-centered-v1';
+const CACHE_NAME = 'bolao-nexa-final-ultimos2-v2';
 
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
